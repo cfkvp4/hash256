428c30315333c3f23b97df37e55123c968284830
cfkvp4 <fkvpwlg@gmail.com>
2024-11-05 15:16:47 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+async function calculateHash() {
+    const text = document.getElementById('inputText').value;
+    const encoder = new TextEncoder();
+    const data = encoder.encode(text);
+    const hashBuffer = await crypto.subtle.digest('SHA-256', data);
+    const hashArray = Array.from(new Uint8Array(hashBuffer));
+    const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
+    document.getElementById('outputHash').value = hashHex;
+}
